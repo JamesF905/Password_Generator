5d7f3d43ff3607c91e6182d36aa7e2d7b5866458
James <jameslfidlin@gmail.com>
2022-05-19 04:00:47 -0400

    second commit - changed javascript test file and test.html file

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -1,93 +1,50 @@
-let possible_characters = ""; //Initialize the possible characters string
-let criteria_selection = [];  //Initialize the criteria selection status array
+//Get the length of the password
+let pass_length = prompt("First, select how many characters you want your Password to have.\n\nPlease choose a number between 8 and 128");
 
-//set password criterial 
-let criteria = [
+//ERROR-HANDLING - if the length doesnt meet the criteria,prompt an error and ask again
+while (pass_length < 8 || pass_length > 128){
+    pass_length = prompt("Error\n\nThe length must be between 8 and 128 characters long, Please Try Again");
+}
+
+let possible_characters = ""; //Initialize the possible_characters string
+let criteria_selection_status = false;  //Initialize the criteria_selection_status
+
+//set the characterTypes array 
+let characterTypes = [
     ["Lowercase Letters", "abcdefghijklmnopqrstuvwxyz"],
     ["Uppercase Letters", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
     ["Numbers", "0123456789"],
     ["Special Characters", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"]
 ];
 
-criteria.forEach(compilePC);
+alert("Next, you must select at least one of the following character types to include in your password.\n\n\nClick \"OK\" for Yes\n\n\"Cancel\" for No");
+
+/* Run a foreach loop on the characterTypes array to get user character type selections, 
+then use those selections to edit both the "possible_characters" string, 
+and the "criteria_selection_status" array*/
+characterTypes.forEach(compilePC); 
 
 function compilePC(Chartype) {
     let status = confirm("Do you want to include "+Chartype[0]);
-    criteria_selection.push([Chartype[0], status]);
     if(status == true){
+    criteria_selection_status = true;
     possible_characters += Chartype[1];
   }
 }
 
-alert(possible_characters);
-alert(criteria_selection);
-
-
-
-
-
-
-function makeid(len) {
-    
-    /*
-    let criteria = {
-        lowercase: ["abcdefghijklmnopqrstuvwxyz", confirm("Do you want to include Lowercase Characters?")],
-        uppercase: [criteria.lowercase.toUpperCase(), confirm("Do you want to include Uppercase Characters?")],
-        numbers: ["0123456789", confirm("Do you want to include Numbers?")],
-        spec: ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", confirm("Do you want to include Special Characters?")]
-      };*/
-    // iterate an array or object
-
-    //send confirms right to a form that updates a global
-
-
-
-    //let l_Case = ["abcdefghijklmnopqrstuvwxyz", confirm("Do you want to include Lowercase Characters?")];
-    //let u_Case = [l_Case[0].toUpperCase(), confirm("Do you want to include Uppercase Characters?")];
-    //let numeric = ["0123456789", confirm("Do you want to include Numbers?")];
-    //let special = ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", confirm("Do you want to include Special Characters?")];
-    /*
-    let password = "";
-    let lowercase = "abcdefghijklmnopqrstuvwxyz";
-    let uppercase = criteria.lowercase.toUpperCase();
-    let numbers = "0123456789";
-    let special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-    */
-/*
-    console.log(criteria);
-
-
-    let possible_characters = "";
-    if(l_Case[1] == true) possible_characters = possible_characters+lowercase
-    if(u_Case[1] == true) possible_characters = possible_characters+uppercase
-    if(numeric[1] == true) possible_characters = possible_characters+numbers
-    if(special[1] == true) possible_characters = possible_characters+special
-    alert(possible_characters);*/
- /*
-    for (let i = 0; i < len; i++)
-   //password += possible_characters. charAt(Math. floor(Math. random() * possible_characters. length));
-   password += possible_characters[Math.floor(Math.random() * possible_characters.length)];
-    
-    let poop = confirm(password);
-    if(poop ==true){ makeid(100);}*/
-  }
-
-  makeid(100)
-
-  // " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
-  /*function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
+//ERROR-HANDLING - If at least one of the Character Types arent selected, prompt an error and ask them to try again 
+while (criteria_selection_status == false){
+    alert("ERROR\n\n You must select at least ONE of the following character types. Please Try Again.");
+    characterTypes.forEach(compilePC);
 }
 
-  makeid(10); */
+//alert(possible_characters);
+//alert(criteria_selection_status);
 
-
-  // possible_characters[Math.floor(Math.random() * possible_characters.length)];
+let generated_password = "";
+for (let i = 0; i < pass_length; i++){
+    generated_password += possible_characters[Math.floor(Math.random() * possible_characters.length)];
+}
+alert("Here is your new Password\n\n"+generated_password);
+document.getElementById('password').innerHTML = generated_password;
+document.getElementById('password').style["opacity"] = "1";
